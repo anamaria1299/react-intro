566b9fb89f01c380e576543b564c59eb08a3a2b2
Rincon Ana <ana.rincon@payulatam.com>
2019-08-27 15:45:55 -0500

    third part finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import './App.css'
-import {TodoList} from "./TodoList"
-import {TodoApp} from "./TodoApp";
+import {TodoApp} from "./TodoApp"
 
 class App extends React.Component {
 
@@ -9,10 +8,10 @@ class App extends React.Component {
 
     const todos = [{text:"Learn React", priority:5, dueDate: (new Date()).toDateString() },
       {text:"Learn about APIs", priority:4, dueDate: (new Date(2018,8,30)).toDateString() },
-      {text:"write TODO App", priority:3, dueDate: (new Date(2018,9,30)).toDateString() }]
+      {text:"write TODO App", priority:3, dueDate: (new Date(2018,-2,30)).toDateString() }]
 
     return (
-      <TodoApp />
+        <TodoApp />
     )
   }
 }
--- src/TodoApp.js
+++ src/TodoApp.js
@@ -3,9 +3,7 @@ import './App.css'
 import {TodoList} from "./TodoList"
 
 export class TodoApp extends React.Component {
-
     constructor(props) {
-
         super(props);
         this.state = { items: [], text: '' };
         this.handleChange = this.handleChange.bind(this);
@@ -13,55 +11,66 @@ export class TodoApp extends React.Component {
     }
 
     render() {
-
         return (
             <div>
                 <h3>TODO</h3>
-                <form onSubmit={this.handleSubmit}>
+                <form onSubmit={this.handleSubmit} id="form">
                     <label htmlFor="new-todo">
                         What needs to be done?
                     </label>
+                    <br/>
+                    <label htmlFor="new-todo">
+                        Text:
+                    </label>
                     <input
-                        id="todo-text"
-                        onChange={this.setText}
+                        id="text"
+                        onChange={this.handleChange}
                         value={this.state.text}
                     />
-                    <input
-                        id="todo-priority"
-                        onChange={this.set}
-                        value={this.state.items.priority}
-                    />
-                    <input
-                        id="todo-dueDate"
-                        onChange={this.handleChange}
-                        value={this.state.items.dueDate}
-                    />
+                    <br/>
+                    <label htmlFor="new-todo">
+                        Priority:
+                    </label>
+                    <input id="priority"/>
+                    <br/>
+                    <label htmlFor="date">
+                        Write the due date (year/month/day) in the following boxes:
+                    </label>
+                    <input id="year" size={5}/>
+                    <input id="month" size={3}/>
+                    <input id="day" size={3}/>
                     <button>
                         Add #{this.state.items.length + 1}
                     </button>
+                    <TodoList items={this.state.items} />
                 </form>
-                <TodoList items={this.state.items} />
             </div>
         );
     }
 
-    setText(e) {
+    handleChange(e) {
         this.setState({ text: e.target.value });
     }
 
     handleSubmit(e) {
-
         e.preventDefault();
         if (!this.state.text.length) {
             return;
         }
+        let year, month, day;
+        year = parseInt(document.getElementById("year").value)
+        month = parseInt(document.getElementById("month").value)
+        day = parseInt(document.getElementById("day").value)
         const newItem = {
             text: this.state.text,
+            priority: document.getElementById("priority").value,
+            dueDate: (new Date(year, month-1, day)).toDateString(),
             id: Date.now()
         };
         this.setState(prevState => ({
             items: prevState.items.concat(newItem),
             text: ''
         }));
+        document.getElementById("form").reset()
     }
 }
